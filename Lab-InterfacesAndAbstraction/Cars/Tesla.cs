7bb065028f32bf19cbcb696d3be2e3b786f0a1c4
Мария Павлова <ivana.prego@gmail.com>
2025-07-14 14:36:41 +0300

    😺

---
+++ Lab-InterfacesAndAbstraction/Cars/Tesla.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Battery { get; set; }
+
+        public Tesla(string model, string color, int battery)
+        {
+            Model=model;
+            Color=color;
+            Battery=battery;
+        }
+
+        public string Start()
+      => "Engine start";
+
+        public string Stop()
+       => "Breaaak!";
+
+        public override string ToString()
+        => $"{Color} Tesla Model {Model} with {Battery} Batteries";
+    }
+}
